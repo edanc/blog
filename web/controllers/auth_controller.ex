8d67ec66e38e7cbc00ca4270640a62d0e322b42a
Ernesto Cojulun <erniecojulun@gmail.com>
2017-07-15 19:17:49 -0700

    no need to assign here
    just store id in session
    issue:
      when hitting the session plug the conn.assigns is empty
      i think its because of the different scopes
      thats why we set it in the call method of Session

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -28,7 +28,6 @@ defmodule Blog.AuthController do
     case find_or_create(user_struct) do
       {:ok, user} ->
         conn = conn
-               |> puts_current_user(user)
                |> put_session(:user_id, user.id)
                |> configure_session(render: true)
 
@@ -38,13 +37,6 @@ defmodule Blog.AuthController do
         |> put_flash(:info, "error signing in")
         |> redirect(to: page_path(conn, :index))
     end
-  end
-
-  defp puts_current_user(conn, user) do
-    token = Phoenix.Token.sign(conn, "user socket", user.id)
-    conn
-    |> assign(:current_user, user)
-    |> assign(:user_token, token)
   end
 
   defp find_or_create(user_struct) do
